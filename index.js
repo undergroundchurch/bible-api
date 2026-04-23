4f65740c3fd3ef0f17c7b6dcea081b597c39ce59
Lucas <ltonussi@deitres.com>
2026-04-23 00:28:23 -0300

    refactor: remove command prefix and update test cases accordingly

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const commandsMeta = require('./CommandHandlers')
 
-const PREFIX = '.'
-
 function ProcessingInstruction(msg) {
-  const command = msg.split(' ')[0].slice(PREFIX.length)
+  const command = msg.split(' ')[0]
   const args = msg.split(' ').slice(1).join(' ')
   return commandsMeta[command](args)
 }
